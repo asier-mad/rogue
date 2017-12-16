4a58511c91fcf57acba0ca5ad77306e7d7b3eef5
Asier <asier@prismacm.com>
2017-12-16 14:12:28 +0100

    Añadido ORM
    ORM: vlucas/spot2

--- web/core/dependencies.php
+++ web/core/dependencies.php
@@ -6,15 +6,30 @@ declare(strict_types = 1);
 
 $injector = new \League\Container\Container();
 
+//Spot (ORM)
+$db = new \Spot\Config();
+$db->addConnection('mysql', [
+    'dbname'   => getenv('DB_NAME'),
+    'user'     => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'host'     => getenv('DB_HOST'),
+    'driver'   => 'pdo_mysql'
+]);
+
+$injector
+    ->add('\Spot\Locator')
+    ->withArgument($db);
+
+//Twig (Templates)
 $injector
     ->add('Twig_Environment')
     ->withArgument(
         new Twig_Loader_Filesystem(__DIR__ . '/../views/')
     );
 
+//Auto-wiring
 $injector
     ->delegate(
-    //Auto-wiring
     new \League\Container\ReflectionContainer()
 );
 
