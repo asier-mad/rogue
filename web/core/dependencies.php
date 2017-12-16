846f56517ca02c8b32c92554c54bff7baad7c367
Asier <asier@prismacm.com>
2017-12-16 14:17:48 +0100

    Mejora datos ORM

--- web/core/dependencies.php
+++ web/core/dependencies.php
@@ -8,13 +8,14 @@ $injector = new \League\Container\Container();
 
 //Spot (ORM)
 $db = new \Spot\Config();
-$db->addConnection('mysql', [
+$datas_db = [
     'dbname'   => getenv('DB_NAME'),
     'user'     => getenv('DB_USER'),
     'password' => getenv('DB_PASS'),
     'host'     => getenv('DB_HOST'),
-    'driver'   => 'pdo_mysql'
-]);
+];
+if(getenv('DB_DRIVER')) $datas_db['driver'] = getenv('DB_DRIVER');
+$db->addConnection(getenv('DB_TYPE'),$datas_db);
 
 $injector
     ->add('\Spot\Locator')
